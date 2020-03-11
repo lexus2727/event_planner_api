b7dcd0b68891ad5296c1290efc80ee1646b63ef9
lexus2727 <lexus2424@yahoo.com>
2020-03-11 17:11:09 -0400

    update

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,36 @@ class Api::V1::EventsController < ApplicationController
         events = Event.all
         render json: events, include: [:tasks] 
     end
+
+    def show
+        event = Event.find(params[:id])
+        render json: event
+    end
+
+    def create
+        event = Event.create(event_params)
+        render json: event, status: 201
+    end
+
+    def update
+        event = Event.find(params[:id])
+        event.update(event_params)
+        render json: event
+    end
+
+    def destroy
+        event = Event.find(params[:id])
+        event.tasks.destroy_all
+        event.destroy
+        render json: event
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:title, :deadline, :description)
+    end
+ 
+
+
 end
---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::TasksController < ApplicationController
+
+    def create
+        task = Task.create(task_params)
+        task.completed = false
+        task.save
+        render json: task, status: 201
+
+    end
+
+    def update
+        task = Task.find(params[:id])
+        task.update(completed: params[:completed])
+        render json: task
+    end
+
+    def destroy
+        task = Task.find(params[:id])
+        task.destroy
+        render json: task
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:deadline, :completed, :description, :event_id)
+    end
+end
