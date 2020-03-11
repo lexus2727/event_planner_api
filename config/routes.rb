56d0d1f3351fe58474490f1621de19d777a9d86a
lexus2727 <lexus2424@yahoo.com>
2020-03-11 15:32:07 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+     namespace :api do 
+      namespace :v1 do
+      
+     
+  resources :events, only: [:show] do
+    resources :tasks, only: [:index]
+  end 
+
+  resources :tasks, only: [:create, :update, :destroy]
+  resources :events
+   end  
+  end
 end
